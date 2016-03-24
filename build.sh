f3f2479641b3a51143c455e87fbf94911d164b2d
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2016-03-24 16:29:58 -0300

    Simplified build script.

--- build.sh
+++ build.sh
@@ -1,26 +1,5 @@
 cd ~/divergente
 drush kw-b
+
 cd build
-drush kw-m
-drush cc all
-drush updb -y
-drush fra -y
-drush clsyn
-if [ -d "profiles/divergente/themes/taller/assets/sass/" ]; then
-  cd profiles/divergente/themes/taller/assets/sass/
-  bundle install
-  bundle exec compass compile
-fi
-cd ~/divergente/build
-if [ -d "profiles/divergente/themes/divergente/assets/sass/" ]; then
-  cd profiles/divergente/themes/divergente/assets/sass/
-  bundle install
-  bundle exec compass compile
-fi
-cd ~/divergente/build
-if [ -d "profiles/divergente/themes/ember" ]; then
-  cd profiles/divergente/themes/ember
-  bundle install
-  bundle exec compass compile
-fi
-cd ~/divergente
+drush kw-u
