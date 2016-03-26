b4409e8b323a3d49c606db4369c18c5c536182b4
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2016-03-25 21:25:23 -0300

    Added globbing to sass.

--- gulp/styles.js
+++ gulp/styles.js
@@ -2,19 +2,35 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
-var config = require('./config');
+var path = require('path');
+var del = require('del');
+var glob = require('node-sass-globbing');
+var assetFunctions = require('node-sass-asset-functions');
 
-gulp.task('styles', function () {
-  return gulp.src(config.theme.path + '/assets/sass/source/**/*')
+var config = require('./config');
+var assets = config.theme.path + '/assets';
+
+gulp.task('styles', ['clean:styles'], function () {
+  return gulp.src(assets + '/sass/source/**/*')
     .pipe(sass({
+      functions: assetFunctions({
+        images_path: assets + '/images',
+        http_images_path: '/' + path.relative('build', assets) + '/images'
+      }),
       includePaths: [
-        config.theme.path + '/assets/sass/library',
-        config.theme.path + '/assets/sass/partials'
-      ]
-    }).on('error', sass.logError))
+        assets + '/sass/library',
+        assets + '/sass/partials'
+      ],
+      importer: glob
+    })
+    .on('error', sass.logError))
     .pipe(autoprefixer({
       browsers: ['last 2 versions', 'ie 9', 'ie 10', 'ie 11'],
       cascade: false
     }))
-    .pipe(gulp.dest(config.theme.path + '/assets/css'));
+    .pipe(gulp.dest(config.theme.path + '/css'));
+});
+
+gulp.task('clean:styles', function () {
+  return del([assets + '/css']);
 });
